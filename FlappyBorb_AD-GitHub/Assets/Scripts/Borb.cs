2a391a81bfcd815f19645b4265a0cdfffffaf1be
Tigerlilly2931 <55060114+Tigerlilly2931@users.noreply.github.com>
2022-02-06 20:39:23 -0600

    Second Push
    Changed up the code: Change Input Manager to take Flap instead of MouseButton0, made the sky scroll slower than the ground, gave # of lives plus invincibility timer so player doesn't die instantly.

--- FlappyBorb_AD-GitHub/Assets/Scripts/Borb.cs
+++ FlappyBorb_AD-GitHub/Assets/Scripts/Borb.cs
@@ -16,6 +16,14 @@ public class Borb : MonoBehaviour
 
     public GameObject GameOverText;
 
+    public int lives = 3;
+
+    private bool invincibleShield = false;
+    private float inviShieldTimer = 0;
+    public int TimerMax = 2;
+
+    public Text livesText;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,14 +32,17 @@ public class Borb : MonoBehaviour
         score = 0;
         ScoreText = GameObject.Find("ScoreTXT").GetComponent<Text>();
         GameOverText.SetActive(false);
+        invincibleShield = false;
+        gameObject.GetComponent<Rigidbody2D>().freezeRotation = true;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        livesText.text = "Extra lives: " + lives;
         if (!isDed)
         {
-            if (Input.GetMouseButtonDown(0))
+            if (Input.GetButton("Flap"))
             {
                 gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
                 gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2 (0, jumpForce));
@@ -43,18 +54,61 @@ public class Borb : MonoBehaviour
         }
         if (isDed)
         {
-            if (Input.GetMouseButtonDown(0))
+            if (Input.GetButton("Flap"))
             {
                 SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
             }
         }
+        if (invincibleShield && inviShieldTimer <= TimerMax)
+        {
+            inviShieldTimer += Time.deltaTime;
+            gameObject.GetComponent<SpriteRenderer>().color = Color.red;
+            //having issues where if I disable the collider and fall to ground floor, timer isn't fast enough to catch dragon. Will setting to isTrigger work?
+            //gameObject.GetComponent<Collider2D>().enabled = false;
+            gameObject.GetComponent<Collider2D>().isTrigger = true;
+        }
+        else if (invincibleShield && inviShieldTimer > TimerMax)
+        {
+            gameObject.GetComponent<SpriteRenderer>().color = Color.white;
+            //gameObject.GetComponent<Collider2D>().enabled = true;
+            gameObject.GetComponent<Collider2D>().isTrigger = false;
+            gameObject.GetComponent<Rigidbody2D>().freezeRotation = true;
+            gameObject.transform.rotation = new Quaternion(0, 0, 0, 0);
+            invincibleShield = false;
+        }
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        gameObject.GetComponent<Rigidbody2D>().freezeRotation = false;
+        if(lives == 0 || collision.gameObject.tag == "Ground")
+        {
+            Died();
+        }
+        else
+        {
+            invincibleShield = true;
+            inviShieldTimer = 0;
+            lives--;
+        }
+
+    }
+    //I'm being picky with this Trigger Enter - I didn't like that the dragon fell through the ground if it hit a collumn and then hit the ground, so I fixed it.
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            Died();
+            inviShieldTimer = 3;
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x, -2.648446f, gameObject.transform.position.z);
+        }
+    }
+
+    private void Died()
+    {
+        gameObject.GetComponent<Collider2D>().isTrigger = false;
         gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
         isDed = true;
         gameObject.GetComponent<Animator>().SetBool("Die", true);
         GameOverText.SetActive(true);
-
     }
 }
--- FlappyBorb_AD-GitHub/Assets/Scripts/ScrollingObject.cs
+++ FlappyBorb_AD-GitHub/Assets/Scripts/ScrollingObject.cs
@@ -7,12 +7,23 @@ public class ScrollingObject : MonoBehaviour
     private Rigidbody2D rb2d;
     public float scrollSpeed = -1.5f;
     public Borb birdy;
+
+    public GameObject sky;
+    private Rigidbody2D skyrb;
+    private float skyScrollSpeed;
     // Start is called before the first frame update
     void Start()
     {
         birdy = GameObject.Find("Dargon").GetComponent<Borb>();
         rb2d = GetComponent<Rigidbody2D>();
         rb2d.velocity = new Vector2(scrollSpeed, 0);
+        if(gameObject.tag == "Obstacle")
+        {
+            return;
+        }
+        skyScrollSpeed = scrollSpeed / 5;
+        skyrb = sky.GetComponent<Rigidbody2D>();
+        skyrb.velocity = new Vector2(skyScrollSpeed, 0);
     }
 
     // Update is called once per frame
@@ -21,6 +32,11 @@ public class ScrollingObject : MonoBehaviour
         if (birdy.isDed)
         {
             rb2d.velocity = Vector2.zero;
+            if(gameObject.tag == "Obstacle")
+            {
+                return;
+            }
+            skyrb.velocity = Vector2.zero;
         }
     }
 }
